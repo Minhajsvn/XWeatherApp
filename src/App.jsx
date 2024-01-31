baeec48adc4aab6e58d3697209fc4bd23c00bb58
minhajsvn <minhajrafeeq7@gmail.com>
2024-01-31 08:47:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+import { useState, useEffect } from 'react';
+import axios from "axios";
+import './App.css'
+
+function App() {
+  const [data, setData] = useState();
+  const [searchText, setSearchText] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+  const [loading, isLoading] = useState(false);
+
+
+  const handleSubmit = () => {
+    setIsSubmit(true);
+  }
+
+  const handleChange = (e) => {
+    setSearchText(e.target.value);
+  }
+
+  const fetchWeather = async (searchText) => {
+    try {
+      isLoading(true);
+      const response = await axios.get(`https://api.weatherapi.com/v1/current.json?key=2f5c79b01d644ca291d10613243101&q=${searchText}`)
+      setData(response.data);
+      isLoading(false)
+    } catch (error) {
+      alert("Failed to fetch weather data");
+    }
+  }
+  useEffect(() => {
+    if(isSubmit == true ){
+    fetchWeather(searchText);
+    setIsSubmit(false);
+    // console.log("data fetched")
+    }
+  },[isSubmit])
+
+  return (
+    <>
+      <div>
+        <input type="text" onChange={handleChange} value={searchText} />
+        <button onClick={handleSubmit}>Submit</button>
+      </div>
+      {data ? 
+          <div className="weather-card">
+            <div className="weather-data">
+              <h3>Temperature</h3>
+              <p>{data.current.temp_c}°C</p>
+            </div>
+            <div className="weather-data">
+              <h3>Humidity</h3>
+              <p>{data.current.humidity}%</p>
+            </div>
+            <div className="weather-data">
+              <h3>Condition</h3>
+              <p>{data.current.condition.text}</p>
+            </div>
+            <div className="weather-data">
+              <h3>Wind Speed</h3>
+              <p>{data.current.wind_kph} kph</p>
+            </div>
+          </div> : loading ? 
+              <div>
+                <p>Loading Data...</p>
+              </div> : ""}
+    </>
+  )
+}
+
+export default App
